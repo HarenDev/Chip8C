e8fb80523058f76165d3e75b9bdfea5fa61b95dd
HarenDev <157764758+HarenDev@users.noreply.github.com>
2024-11-22 14:29:31 -0400

    renamed, also finished adding the necesary variables, started working on the init function

---
+++ src/Chip8.cpp
@@ -0,0 +1,17 @@
+#include "Chip8.h"
+
+void Chip8::doInit(){
+    pc = 0x200;
+    opcode = 0;
+    I = 0;
+    sp = 0;
+
+    for (int i = 0; i < 80; ++i){
+        
+    }
+}
+
+void Chip8::doCycle(){
+
+}
+
